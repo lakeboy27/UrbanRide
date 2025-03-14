65ca91cdfeeb20519850996721ca26e26a04cb93
githubanujmishra <anujmishra2702@gmail.com>
2025-03-14 13:05:27 +0530

    Finding Trip And Creating Rides

--- backend/routes/ride.routes.js
+++ backend/routes/ride.routes.js
@@ -1,6 +1,6 @@
 const express = require('express') ; 
 const router = express.Router() ; 
-const {body} = require('express-validator') ; 
+const {body , query} = require('express-validator') ; 
 const rideController = require('../controllers/ride.controller') ; 
 const authMiddleware = require('../middlewares/auth.middleware') ; 
 
@@ -10,6 +10,13 @@ router.post('/create', authMiddleware.authUser ,
     body('vehicleType').isString().isIn(['auto' , 'car' , 'moto']).withMessage('Invalide vehicle type') , 
     rideController.createRide 
 )
+
+router.get('/getfare' , authMiddleware.authUser , 
+    query('pickup').isString().isLength({ min: 3 }).withMessage('Pickup location must be a string'),
+    query('destination').isString().isLength({ min: 3 }).withMessage('Destination must be at least 3 characters long'),
+    rideController.getFare
+)
+
 
 
 module.exports = router ; 
